bb0507a3c6ef88a72531a11cf518339129cb4db2
Kawsar Ahmed <mkawsar10@gmail.com>
2023-07-19 09:54:00 -0400

    Add files via upload

---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<div
+	class="w-screen flex flex-col gap-y-10 justify-center items-center mx-auto pt-10 h-screen bg-gray-800"
+>
+	<h1 class="uppercase hover:scale-110 text-yellow-500 bg-gray-800 p-5 rounded-sm text-4xl">
+		sheba queue inc.
+	</h1>
+	<img class="hover:scale-110" src="/logo.png" alt="" />
+	<p class="text-2xl uppercase text-teal-700">Landing page developing...</p>
+</div>
+
+<svelte:head>
+	<title>Sheba Queue | Software</title>
+</svelte:head>
