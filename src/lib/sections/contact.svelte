8d8707797352d89e325490654bac7abbf8a013cf
Kawsar Ahmed <prokawsar@gmail.com>
2023-07-31 17:24:37 +0600

    contact us ui

--- src/lib/sections/contact.svelte
+++ src/lib/sections/contact.svelte
@@ -1,13 +1,164 @@
 <script>
 </script>
 
-<div id="contact" class="bg-slate-700 pt-10 w-full">
-  <div class="container mx-auto flex flex-col sm:flex-row text-white">
-    <div class="flex-1">
-      <h1 class="uppercase text-5xl sm:text-8xl">contact</h1>
+<div id="contact" class="bg-gray-100 py-10 w-full relative">
+  <div
+    class="container mx-auto flex flex-col justify-center sm:flex-row text-white"
+  >
+    <div class="flex justify-center">
+      <div class="lg:w-7/12 text-center">
+        <h1 class="text-5xl/relaxed text-gray-700">Contact Us</h1>
+        <p class="mb-6 md:text-lg text-gray-500">
+          Please fill out the following form and we will get back to you shortly
+        </p>
+      </div>
     </div>
-    <div class="flex-1">
-      <div>Svelte</div>
+  </div>
+
+  <div class="absolute -bottom-1 w-full">
+    <svg
+      class="w-full h-full"
+      viewBox="0 0 1440 40"
+      version="1.1"
+      xmlns="http://www.w3.org/2000/svg"
+      xmlns:xlink="http://www.w3.org/1999/xlink"
+    >
+      <g
+        id="shape-b"
+        stroke="none"
+        stroke-width="1"
+        fill="none"
+        fill-rule="evenodd"
+      >
+        <g id="curve" fill="#fff">
+          <path
+            d="M0,30.013 C239.659,10.004 479.143,0 718.453,0 C957.763,0 1198.28,10.004 1440,30.013 L1440,40 L0,40 L0,30.013 Z"
+            id="Path"
+          />
+        </g>
+      </g>
+    </svg>
+  </div>
+</div>
+
+<div class="flex w-full">
+  <div
+    class="lg:flex items-center container px-10 mx-auto flex-row w-full justify-center"
+  >
+    <div class="lg:w-1/2">
+      <div class="mb-6 relative bg-clip-border rounded-[0.1875rem]">
+        <div class="py-12">
+          <h2 class="mb-4 text-2xl/6 mt-0 font-medium">Let's Talk Further</h2>
+          <p class="mb-12 text-base/6">
+            Please fill out the following form and we will get back to you
+            shortly
+          </p>
+
+          <form>
+            <div class="flex gap-6">
+              <div class="md:w-1/2">
+                <div class="mb-5">
+                  <label
+                    for="firstname"
+                    class="block text-sm font-medium mb-1 text-gray-600"
+                    >First Name <span class="text-red-500">*</span></label
+                  >
+                  <input
+                    type="text"
+                    class="py-2 px-4 leading-6 block w-full border border-gray-300 rounded text-sm focus:border-gray-300 focus:ring-0"
+                    id="firstname"
+                    placeholder="First Name"
+                  />
+                </div>
+              </div>
+              <div class="md:w-1/2">
+                <div class="mb-5">
+                  <label
+                    for="lastname"
+                    class="block text-sm font-medium mb-1 text-gray-600"
+                    >Last Name <span class="text-red-500">*</span></label
+                  >
+                  <input
+                    type="text"
+                    class="py-2 px-4 leading-6 block w-full border border-gray-300 rounded text-sm focus:border-gray-300 focus:ring-0"
+                    id="lastname"
+                    placeholder="Last Name"
+                  />
+                </div>
+              </div>
+            </div>
+            <div class="w-full">
+              <div class="mb-5">
+                <label
+                  for="email"
+                  class="block text-sm font-medium mb-1 text-gray-600"
+                  >Email <span class="text-red-500">*</span></label
+                >
+                <input
+                  type="email"
+                  class="py-2 px-4 leading-6 block w-full border border-gray-300 rounded text-sm focus:border-gray-300 focus:ring-0"
+                  id="email"
+                  placeholder="Your Email"
+                />
+              </div>
+              <div class="mb-5">
+                <label
+                  for="message"
+                  class="block text-sm font-medium mb-1 text-gray-600"
+                  >Message <span class="text-red-500">*</span></label
+                >
+                <textarea
+                  class="py-2 px-4 leading-6 block w-full border border-gray-300 rounded text-sm focus:border-gray-300 focus:ring-0"
+                  id="message"
+                  rows="4"
+                  placeholder="Type Your Massage..."
+                />
+              </div>
+            </div>
+            <button
+              type="submit"
+              class="inline-flex items-center bg-violet-500 text-white text-sm font-medium leading-6 text-center align-middle select-none py-2 px-4 rounded-md transition-all hover:shadow-lg"
+            >
+              Send
+              <span class="w-4 h-4 ms-1">
+                <svg
+                  class="w-full h-full text-white"
+                  viewBox="0 0 24 24"
+                  version="1.1"
+                  xmlns="http://www.w3.org/2000/svg"
+                  xmlns:xlink="http://www.w3.org/1999/xlink"
+                >
+                  <g
+                    stroke="none"
+                    stroke-width="1"
+                    fill="none"
+                    fill-rule="evenodd"
+                  >
+                    <rect id="bound" x="0" y="0" width="24" height="24" />
+                    <path
+                      d="M3,13.5 L19,12 L3,10.5 L3,3.7732928 C3,3.70255344 3.01501031,3.63261921 3.04403925,3.56811047 C3.15735832,3.3162903 3.45336217,3.20401298 3.70518234,3.31733205 L21.9867539,11.5440392 C22.098181,11.5941815 22.1873901,11.6833905 22.2375323,11.7948177 C22.3508514,12.0466378 22.2385741,12.3426417 21.9867539,12.4559608 L3.70518234,20.6826679 C3.64067359,20.7116969 3.57073936,20.7267072 3.5,20.7267072 C3.22385763,20.7267072 3,20.5028496 3,20.2267072 L3,13.5 Z"
+                      id="Combined-Shape"
+                      fill="currentcolor"
+                    />
+                  </g>
+                </svg>
+              </span>
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+
+    <div class="lg:w-5/12 ms-auto overflow-x-hidden">
+      <div class="h-[520px]">
+        <div id="marker-map5" class="h-100" data-toggle="map">
+          <iframe
+            class="w-full h-[500px]"
+            src="https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d6030.418742494061!2d-111.34563870463673!3d26.01036670629853!3m2!1i1024!2i768!4f13.1!5e0!3m2!1ses-419!2smx!4v1471908546569"
+            frameborder="0"
+          />
+        </div>
+      </div>
     </div>
   </div>
 </div>
