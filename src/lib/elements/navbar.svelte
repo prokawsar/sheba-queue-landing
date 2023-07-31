bb078345d68d67d7da458b32a15a330e758e4573
Kawsar Ahmed <prokawsar@gmail.com>
2023-07-31 18:27:28 +0600

    namvar wip

--- src/lib/elements/navbar.svelte
+++ src/lib/elements/navbar.svelte
@@ -1,19 +1,47 @@
-<div class="bg-gray-100">
+<script>
+  import Menu from "$lib/icons/menu.svelte";
+
+  let showMobileMenu = false;
+
+  const openMenu = () => {
+    showMobileMenu = !showMobileMenu;
+  };
+</script>
+
+<div class="bg-gray-100 fixed top-0 w-full z-20">
   <div
-    class="container mx-auto flex flex-row w-full justify-between items-center"
+    class="container mx-auto flex flex-row w-full sm:justify-between items-center"
   >
-    <div class="w-52">
-      <img src="/logo.png" alt="logo" class="p-2" />
+    <!-- svelte-ignore a11y-no-static-element-interactions -->
+    <div class="sm:hidden mr-2" on:click={() => openMenu()} on:keydown>
+      <Menu />
     </div>
-    <div class="flex flex-row gap-4 sm:-ml-10">
+    <div class="w-52">
+      <img src="/images/logo.png" alt="logo" class="p-2" />
+    </div>
+    <div class="hidden sm:flex flex-row gap-4 sm:-ml-10">
       <a class="" href="/">Home</a>
-      <a href="/#about">About</a>
-      <a href="/#team">Team</a>
+      <!-- <a href="/#about">About</a> -->
       <a href="/#our-tech">Our Technology</a>
+      <a href="/#team">Team</a>
       <a href="/#contact">Contact</a>
     </div>
-    <button class="rounded-3xl border-[1.5px] px-3 py-2 border-gray-300">
+    <button
+      class="hidden sm:block rounded-3xl border-[1.5px] px-3 py-2 border-gray-300"
+    >
       Contact Us
     </button>
+
+    {#if showMobileMenu}
+      <div class="absolute left-0 w-full h-60 p-4 bg-gray-300 top-16">
+        <div class="flex flex-col gap-4">
+          <a class="" href="/">Home</a>
+          <!-- <a href="/#about">About</a> -->
+          <a href="/#our-tech">Our Technology</a>
+          <a href="/#team">Team</a>
+          <a href="/#contact">Contact</a>
+        </div>
+      </div>
+    {/if}
   </div>
 </div>
